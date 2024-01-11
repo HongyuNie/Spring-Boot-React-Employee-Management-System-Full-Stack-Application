2d7dd5cdf39e1b242ed1f90a5ccf33113afc25b2
hongyu <nhy0613@outlook.com>
2024-01-11 09:26:45 -0800

    consume get employees rest api from react app using axios, populate the data using useEffect hook

--- ems-frontend/src/components/ListEmployeeComponent.jsx
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,24 +1,18 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { listEmployees } from '../services/EmployeeService';
 
 const ListEmployeeComponent = () => {
-    const dummyData = [
-        {
-            "id":1,
-            "fistName":"Alice",
-            "lastName":"alice@gmail.com"
-        },
-        {
-            "id":2,
-            "fistName":"Ali",
-            "lastName":"ali@gmail.com"
-        },
-        {
-            "id":3,
-            "fistName":"Alison",
-            "lastName":"alison@gmail.com"
-        }
-    ]
-    
+
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        listEmployees().then(response=>{
+            setEmployees(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+    }, [])
+
   return (
     <div className='container'>
         <h2 className='text-center'>List of Employees</h2>
@@ -34,10 +28,10 @@ const ListEmployeeComponent = () => {
             </thead>
             <tbody>
                 {
-                    dummyData.map(employee => 
+                    employees.map(employee => 
                                     <tr key={employee.id}>
                                         <td>{employee.id}</td>
-                                        <td>{employee.fistName}</td>
+                                        <td>{employee.firstName}</td>
                                         <td>{employee.lastName}</td>
                                         <td>{employee.email}</td>
                                     </tr>)
