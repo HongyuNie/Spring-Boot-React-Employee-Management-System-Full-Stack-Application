306188fb821143ddf80f5e540f382bc30b04c84c
hongyu <nhy0613@outlook.com>
2024-01-13 11:55:50 -0800

    connect to update employee rest api

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { createEmployee, getEmployee } from '../services/EmployeeService';
+import { createEmployee, getEmployee,updateEmployee } from '../services/EmployeeService';
 import {useNavigate, useParams} from 'react-router-dom'
 
 
@@ -31,16 +31,24 @@ const EmployeeComponent = () => {
   }, [id])
 
 
-  function saveEmployee(e) {
+  function saveOrUpdateEmployee(e) {
     e.preventDefault();
 
     if (validataForm()) {
       const employee = {firstName, lastName, email};
-    
-      createEmployee(employee).then((response) => {
-        console.log(response.data);
-        navigator('/employees');
-      })
+
+      if (id) {
+        updateEmployee(id, employee).then((response) => {
+          console.log(response.data);
+          navigator('/employees');
+        }).catch(error=>console.log(error));
+      } else {
+        createEmployee(employee).then((response) => {
+          console.log(response.data);
+          navigator('/employees');
+        }).catch(error=>console.log(error));
+      }
+      
     }
   }
 
@@ -130,7 +138,7 @@ const EmployeeComponent = () => {
                   {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
               </div>
 
-              <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+              <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Submit</button>
             </form>
           </div>
         </div>
