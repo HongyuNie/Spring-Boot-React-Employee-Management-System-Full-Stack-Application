e3d6b4e1c0a1cad87d360a71907366a296dc3957
hongyu <nhy0613@outlook.com>
2024-01-21 11:01:02 -0800

    add nav links, create ListDepartment component and configure its route

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -5,6 +5,7 @@ import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import EmployeeComponent from './components/EmployeeComponent'
+import ListDepartmentComponent from './components/ListDepartmentComponent'
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route path='/employees' element={<ListEmployeeComponent/>}></Route>
           <Route path='/add-employee' element={<EmployeeComponent/>}></Route>
           <Route path='/edit-employee/:id' element={<EmployeeComponent/>}></Route>
+          <Route path='/departments' element={<ListDepartmentComponent/>}></Route>
         </Routes>
         <FooterComponent/>
       </BrowserRouter>
